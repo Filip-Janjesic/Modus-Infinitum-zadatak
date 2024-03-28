807726eb23abf7cf7df4134c1fcd123a2e4fb88b
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-28 22:02:30 +0100

    Update MainCommand.php

--- mi-test/app/src/Infrastructure/MainCommand.php
+++ mi-test/app/src/Infrastructure/MainCommand.php
@@ -4,12 +4,20 @@ namespace App\Infrastructure;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'filter')]
 class MainCommand extends Command
 {
+    protected function configure()
+    {
+        $this
+            ->setDescription('Filter dataset based on provided criteria')
+            ->addArgument('type', InputArgument::REQUIRED, 'Type of analysis to perform');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         return Command::SUCCESS;
