27c4753bc2c6fabcee3796828e918d2ad69b847c
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-28 22:03:38 +0100

    Update Filter.php

--- mi-test/app/src/Infrastructure/Filter.php
+++ mi-test/app/src/Infrastructure/Filter.php
@@ -6,7 +6,6 @@ trait Filter
 {
     public function filterByAge($data)
     {
-        // Filter persons older than 20 and younger than 60
         return array_filter($data, function($person) {
             $age = (new \DateTime())->diff(new \DateTime($person['birthday']))->y;
             return $age > 20 && $age < 60;
