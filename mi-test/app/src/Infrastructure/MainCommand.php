e724cc16cf21d96f32e9b6883a15c7c378bbd646
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-29 22:02:14 +0100

    updates

--- mi-test/app/src/Infrastructure/MainCommand.php
+++ mi-test/app/src/Infrastructure/MainCommand.php
@@ -1,12 +1,12 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+
+namespace App\Infrastructure;
+
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use App\Infrastructure\Filter;
-
 
 #[AsCommand(name: 'filter')]
 class MainCommand extends Command
