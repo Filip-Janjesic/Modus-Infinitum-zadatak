ea1f32b032a5de78d9738fedeae187d65aa6c70c
Filip-Janjesic <filip.janjesic@gmail.com>
2024-03-29 21:46:43 +0100

    updates

--- mi-test/app/tests/Infrastructure/MainCommandTest.php
+++ mi-test/app/tests/Infrastructure/MainCommandTest.php
@@ -1,13 +1,27 @@
 <?php
-
+use PHPUnit\Framework\TestCase;
+use App\Infrastructure\MainCommand;
 namespace App\Tests\Infrastructure;
 
 use PHPUnit\Framework\TestCase;
+use App\Infrastructure\MainCommand;
 
 class MainCommandTest extends TestCase
 {
-    public function testLookupThrowsExceptionWhenNoMapping(): void
+    public function testFilterByAge(): void
     {
-        $this->expectException(\RuntimeException::class);
+        
+        $mainCommand = new MainCommand();
+        $data = [
+            ['Nname' => 'John', 'Bbirthday' => '2000-01-01'], 
+            ['Nname' => 'Jane', 'Bbirthday' => '1980-01-01'], 
+            ['Nname' => 'Adam', 'Bbirthday' => '1960-01-01'], 
+        ];
+        $expectedResult = [
+            ['Nname' => 'John', 'Bbirthday' => '2000-01-01'],
+            ['Nname' => 'Jane', 'Bbirthday' => '1980-01-01'],
+        ];
+        $filteredData = $mainCommand->filterByAge($data);
+        $this->assertEquals($expectedResult, $filteredData);
     }
 }
